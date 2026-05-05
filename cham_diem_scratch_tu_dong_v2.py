05c226ca6d85aa3324ae1674b3a542c02c875e92
kimhong061388-lgtm <kimhong061388@gmail.com>
2026-05-05 10:31:30 +0700

    Update cham_diem_scratch_tu_dong_v2.py

--- cham_diem_scratch_tu_dong_v2.py
+++ cham_diem_scratch_tu_dong_v2.py
@@ -22,7 +22,7 @@ with st.sidebar:
     st.image("https://flaticon.com", width=80)
     st.title("📖 HƯỚNG DẪN")
     st.info("1. Nhập thông tin\n2. Chọn đúng Đề thi\n3. Tải file .sb3\n4. Nhấn Nộp bài")
-    st.warning("⚠️ CHỈ NỘP 01 LẦN.\nĐề 1: I = L / W\nĐề 2: V = S / T")
+    st.warning("⚠️ CHỈ NỘP 01 LẦN.")
 
 def chuan_hoa(van_ban):
     if not van_ban: return ""
